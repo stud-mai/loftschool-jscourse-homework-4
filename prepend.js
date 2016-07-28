15348ceecb2b5531ffbc2d8ca400efa7e5b041ab
Evgeny <e.a.zhuchkov@gmail.com>
2016-07-28 19:48:50 +0300

    Some changes/enhancуments

--- prepend.js
+++ prepend.js
@@ -1,5 +1,9 @@
 let prepend = (param1,param2) => {
-	param1.insertBefore(param2,param1.childNodes[0]);
+	if (param1.childNodes[0]) {
+		param1.insertBefore(param2, param1.childNodes[0])
+	} else {
+		param1.appendChild(param2)
+	}
 }
 
 module.exports = prepend;
